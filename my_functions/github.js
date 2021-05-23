74f3415034c09af59312317aad45475230fd13e6
vinuch <vincentedeh42@gmail.com>
2021-05-23 01:43:25 -0700

    add missing curly brace to netlify function

--- my_functions/github.js
+++ my_functions/github.js
@@ -60,7 +60,7 @@ exports.handler = function(event, context, callback) {
         callback(null, {
           statusCode: 200,
           headers,
-          body: JSON.stringify(data)
+          body: user
         })
       })
     };
